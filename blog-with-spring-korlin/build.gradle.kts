5c9308dec0d57fb40ff41cba6626adea4d0126b7
sanhee <68936@naver.com>
2022-02-15 02:16:58 +0900

    feat: 스프링부트, 코틀린을 이용한 간단 블로그
    https://spring.io/guides/tutorials/spring-boot-kotlin/

--- blog-with-spring-korlin/build.gradle.kts
+++ blog-with-spring-korlin/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
     id("org.springframework.boot") version "2.6.3"
@@ -6,6 +7,14 @@ plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
     kotlin("plugin.jpa") version "1.6.10"
+    kotlin("plugin.allopen") version "1.4.32"
+    kotlin("kapt") version "1.4.32"
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
 }
 
 group = "com.spring-boot-kotlin"
@@ -30,6 +39,15 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
--- blog-with-spring-korlin/src/main/kotlin/com/springbootkotlin/blog/BlogApplication.kt
+++ blog-with-spring-korlin/src/main/kotlin/com/springbootkotlin/blog/BlogApplication.kt
@@ -1,9 +1,12 @@
 package com.springbootkotlin.blog
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class BlogApplication
 
 fun main(args: Array<String>) {
